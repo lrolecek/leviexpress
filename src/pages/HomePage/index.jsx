30f5b54f630d5ab3baf1e05dda7f69b741d4f82c
Luděk Roleček <ludek@rolecek.cz>
2023-11-11 12:13:32 +0100

    Zobrazeni sedadla

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { JourneyPicker } from '../../components/JourneyPicker';
 import { JourneyDetail } from '../../components/JourneyDetail';
+import { SelectedSeat } from '../../components/SelectedSeat';
 
 export const HomePage = () => {
   const [journey, setJourney] = useState(null);
@@ -16,7 +17,10 @@ export const HomePage = () => {
       <JourneyPicker onJourneyChange={handleJourneyChange} />
 
       { journey !== null
-        ? <JourneyDetail journey={journey} />
+        ? <>
+            <JourneyDetail journey={journey} />
+            <SelectedSeat number={journey.autoSeat} />
+          </>
         : null
       }
     </main>
